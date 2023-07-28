65810750d8e929c46d9b5abf609e668310db5a9a
iRobot98 <matendejudah@gmail.com>
2023-07-28 16:32:58 +0300

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -4,7 +4,7 @@ import { Inter } from "next/font/google";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: "Sunflower App",
+  title: "Sunflower",
   description: "Sunflower app website",
 };
 
