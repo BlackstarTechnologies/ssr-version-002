32e37a1cb30aba94bfc3b2e037ba41eb7c4ba173
iRobot98 <matendejudah@gmail.com>
2023-07-28 16:44:08 +0300

    working on app

---
+++ app/About/page.js
@@ -0,0 +1,7 @@
+export default function About() {
+  return (
+    <main className="flex justify-center items-center h-screen w-screen">
+      <div>About Page</div>
+    </main>
+  );
+}
---
+++ app/auth/sign_up/page.js
@@ -0,0 +1,7 @@
+export default function SignUp() {
+  return (
+    <main className="flex justify-center items-center h-screen w-screen">
+      <div>Sign Up</div>
+    </main>
+  );
+}
