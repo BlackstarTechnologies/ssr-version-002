c76d418e293525d588fcc495fb884855fa741971
iRobot98 <matendejudah@gmail.com>
2023-07-31 13:39:31 +0000

    working on app

--- next.config.js
+++ next.config.js
@@ -1,10 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  reactStrictMode: true,
   swcMinify: true,
+  staticPageGenerationTimeout: 3,
   experimental: {
     appDir: true,
     serverComponentsExternalPackages: ["mongoose"],
   },
 };
-
 module.exports = nextConfig;
